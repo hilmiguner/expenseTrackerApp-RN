3fba0d78b21d75a762bf735fa032cc8f16d73001
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-07-18 10:14:31 +0300

    Commit

--- App.js
+++ App.js
@@ -4,9 +4,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import Ionicons from "react-native-vector-icons/Ionicons";
+
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
+
+import { GlobalStyles } from "./constants/styles";
 
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
@@ -14,10 +18,33 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
   return(
     <BottomTabs.Navigator screenOptions={{
-      
+      headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      headerTintColor: "white",
+      tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      tabBarActiveTintColor: GlobalStyles.colors.accent500,
     }}>
-      <BottomTabs.Screen name="AllExpense" component={AllExpenses}/>
-      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses}/>
+      <BottomTabs.Screen 
+      name="AllExpense" 
+      component={AllExpenses}
+      options={{
+        title: "All Expenses",
+        tabBarLabel: "All Expenses",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="calendar" size={size} color={color}/>
+        )
+      }} 
+      />
+      <BottomTabs.Screen 
+      name="RecentExpenses"
+      component={RecentExpenses}
+      options={{
+        title: "Recent Expenses",
+        tabBarLabel: "Recent",
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="hourglass" size={size} color={color}/>
+        )
+      }} 
+      />
     </BottomTabs.Navigator>
   );
 }
