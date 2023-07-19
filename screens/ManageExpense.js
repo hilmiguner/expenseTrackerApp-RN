215c6f2c1b2117b7511c4c6b53e629b0145cf2d6
Hilmi GUNER <hilmiguner@Hilmi-MacBook-Air.local>
2023-07-19 17:17:16 +0300

    commit

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 import { useContext, useLayoutEffect } from "react";
 import IconButton from "../UI/IconButton";
